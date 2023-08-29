6671bf1b707cd9363d499ecbca7b1423754b1fb7
Judhunja <judxhunja@gmail.com>
2023-08-29 17:33:14 +0300

    insert left fails if parent value is NULL

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	newnode = malloc(sizeof(binary_tree_t));
 
-	if (parent == NULL || newnode == NULL)
+	if (!parent->n || parent == NULL || newnode == NULL)
 		return (NULL);
 
 	newnode->n = value;
