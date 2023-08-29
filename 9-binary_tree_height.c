7791eab8d04a2e1f8685f663140625bdb9e360b8
Judhunja <judxhunja@gmail.com>
2023-08-30 01:23:48 +0300

    don't check left or right tree if they are NULL

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -18,9 +18,24 @@ size_t max(size_t a, size_t b)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left;
+	size_t right;
+
+	left = 0;
+	right = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	return (max(binary_tree_height(tree->left),
-				binary_tree_height(tree->right)));
+	if (tree->left != NULL)
+		left = binary_tree_height(tree->left);
+	else
+		left = 0;
+
+	if (tree->right != NULL)
+		binary_tree_height(tree->right);
+	else
+		right = 0;
+
+	return (max(left + 1, right + 1));
 }
