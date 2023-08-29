8786a7c6f66c67fef4352507125ae10c214a6910
Judhunja <judxhunja@gmail.com>
2023-08-29 23:46:48 +0300

    height of binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -22,5 +22,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	return (max(binary_tree_height(tree->left),
-				binary_tree_height(tree->right)) + 1);
+				binary_tree_height(tree->right)));
 }
