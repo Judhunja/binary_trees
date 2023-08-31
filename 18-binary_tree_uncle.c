92fff3241a588af999fb05229bae5bbe99624b1d
Judhunja <judxhunja@gmail.com>
2023-08-31 03:26:56 +0300

    finding uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -13,14 +13,14 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 			node->parent->parent->right == NULL)
 		return (NULL);
 
-	if (node->parent->left == node)
-	{
-		return (node->parent->parent->left);
-	}
-
-	if (node->parent->right == node)
+	if (node->parent == node->parent->parent->left)
 	{
 		return (node->parent->parent->right);
+	}
+
+	if (node->parent == node->parent->parent->right)
+	{
+		return (node->parent->parent->left);
 	}
 	return (NULL);
 }
